e9f755246637a8539749421b098a7ba703c360e8
Ivanka1234 <gerasymyuk_az15@nuwm.edu.ua>
2025-04-26 21:11:20 +0300

    Перше завантаження проекту

---
+++ OlenaDimaBud/settings.py
@@ -0,0 +1,17 @@
+SECRET_KEY = 'fake-key'
+DEBUG = True
+ALLOWED_HOSTS = []
+INSTALLED_APPS = ['django.contrib.staticfiles']
+MIDDLEWARE = []
+ROOT_URLCONF = 'OlenaDimaBud.urls'
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': ['templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {},
+    },
+]
+WSGI_APPLICATION = 'OlenaDimaBud.wsgi.application'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = ['static']
